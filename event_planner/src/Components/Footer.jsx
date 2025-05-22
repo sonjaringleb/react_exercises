13871f84de3e5f8733d18e18e0fe3679bd464d89
Sonja Ringleb <Sonja.Ringleb@gmail.com>
2025-05-22 16:04:40 -0400

    Add Event Planner

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './EventPlanner.css'; // Import CSS file for styling
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <section class="companyInfo">
+                    <h1>SuperEvents GmbH</h1>
+                    <p>Hauptstr. 1, 10115 Berlin</p>
+                    <p>www.superevents.de</p>
+                </section>
+                <p>&copy; SuperEvents. All rights reserved.</p>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
